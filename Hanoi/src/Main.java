642bb0285153adb94f4d68bcfd91351310251f72
Timothy Chandler <24783526+Ernaldis@users.noreply.github.com>
2020-01-29 10:37:37 -0600

    Extra documentation

--- Hanoi/src/Main.java
+++ Hanoi/src/Main.java
@@ -1,5 +1,19 @@
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * <h1>Modified Towers of Hanoi</h1>
+ * The assignment is as follows:
+ * Solve the towers of Hanoi game for the following graph = (V ,E)
+ * V = {Start, Aux1, Aux2, Aux3, Aux4, Dest}
+ * E = {(Start, Aux1), (Aux1, Aux2), (Aux2, Aux3), (Aux3, Aux4), (Aux4, Aux1), (Aux1, Dest)}
+ * (a) Design an algorithm and determine the time and space complexities of moving n disks from Start to Dest.
+ * (b) Implement this algorithm whereby your program prints out each of the moves of every disk.
+ * Show the output for n = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+ *
+ * @author Timothy Chandler
+ * @since 29/01/2020
+ * @version 1.0
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -11,23 +25,21 @@ public class Main {
         Node node4 = new Node(null, "Node 4");
         Node dest = new Node(null, "destination");
 
+        //next nodes are used to move disks more easily
         start.next = node1;
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
         node4.next = node1;
 
+        //placing the nodes in an array makes passing them as a parameter of the game functions easier
         Node[] node = {start, node1, node2, node3, node4, dest};
 
         //play the first 10 games
-        /*for (int i = 1; i <  11; i++){
+        for (int i = 1; i <  11; i++){
             playGame(i, node);
-            for (int j = 0; j < 6; j++) {
-                node[j].stack.clear();
-            }
-        }*/
-        playGame(10, node);
-
+            node[5].stack.clear();
+        }
     }
 
     /**
@@ -111,18 +123,25 @@ public class Main {
 }
 /*
 move across(n):
-    move across(n-1)
-    tick the nth disk
-    move across(n-1)
-    tick the nth disk
-    move across(n-1)
+    if n=1: move it twice along the cycle
+    otherwise:
+        move across(n-1)
+        tick the nth disk
+        move across(n-1)
+        tick the nth disk
+        move across(n-1)
+
+game is played in two stages:
+    loading
+    unloading
 
 Loading stage:
-    while disk on start:
+    while disks on start:
         load top disk into 3
         move across (size of 4)
         unload 3 into 4
         move across (size of 2)
+
 Unloading stage:
     while disks in cycle:
         move across ((size of 4)-1)
--- Hanoi/src/Node.java
+++ Hanoi/src/Node.java
@@ -1,10 +1,15 @@
 import java.util.Stack;
 
 public class Node {
-    Stack<Integer> stack;
-    Node next;
-    String name;
+    Stack<Integer> stack; //this stack represents the contents of a peg in the game. Disks must be ints
+    Node next; //this will be used to more easily move nodes around the cycle
+    String name; //this is used to denote the specific node
 
+    /**
+     * Initializer for Node
+     * @param next the next node in the cycle
+     * @param name the name of the node
+     */
     Node(Node next, String name){
         this.next = next;
         this.name = name;
