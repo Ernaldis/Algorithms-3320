82bc154f4494c8cd232de3986cb1240b8829f74f
Timothy Chandler <24783526+Ernaldis@users.noreply.github.com>
2020-04-01 10:57:43 -0500

    Optimal Huffman code, modified from geeksforgeeks code

---
+++ Assignment2/Programming/Problem5/src/com/company/HuffmanNode.java
@@ -0,0 +1,12 @@
+package com.company;
+
+// node class is the basic structure
+// of each node present in the Huffman - tree.
+class HuffmanNode {
+
+    int data;
+    char c;
+
+    HuffmanNode left;
+    HuffmanNode right;
+}
